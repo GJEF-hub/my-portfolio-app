eeb4d2f36537b58b2592071e2672d996e2947104
GJEF Specials <gjefict@gmail.com>
2022-06-20 00:56:24 +0100

    mobile version of hor scroll with modifications

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -2,11 +2,8 @@ import React from "react";
 import "./Home.css";
 import "../../assets/buttons/buttons.css";
 import Profile from "../Profile/Profile";
-
 import Contact from "../ContactMe/Contact";
-
 import Testimonials from "../Testimonial/Testimonials";
-
 import Works from "../Works/Works";
 
 export default function Home() {
